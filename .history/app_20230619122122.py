dc5b418438152e83496d3560bae4c097d8a62f70
Nixon <nixonlauture@yahoo.com>
2023-06-20 09:03:52 -0400

    first commit

---
+++ .history/app_20230619122122.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+
+# MongoDB configuration
+MONGO_URI = os.environ.get('https://us-east-1.aws.data.mongodb-api.com/app/data-szqij/endpoint/data/v1')  # Replace with your Atlas MongoDB URI
+DB_NAME = 'fileuploads'  # Replace with your database name
+COLLECTION_NAME = 'uploads'  # Replace with your collection name
+
+# Connect to MongoDB
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+collection = db[COLLECTION_NAME]
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Get the uploaded file
+        file = request.files['file']
+
+        # Save the file to MongoDB
+        file_id = collection.insert_one({'filename': file.filename, 'data': file.read()}).inserted_id
+
+        return f'File uploaded with ID: {file_id}'
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ .history/app_20230619124745.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+
+# MongoDB configuration
+MONGO_URI = os.environ.get('mongodb+srv://nixon:Maya100$@cluster0.wuy0l.mongodb.net/?retryWrites=true&w=majority')  # Replace with your Atlas MongoDB URI
+DB_NAME = 'logging_db'  # Replace with your database name
+COLLECTION_NAME = 'logging_db'  # Replace with your collection name
+
+# Connect to MongoDB
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+collection = db[COLLECTION_NAME]
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Get the uploaded file
+        file = request.files['file']
+
+        # Save the file to MongoDB
+        file_id = collection.insert_one({'filename': file.filename, 'data': file.read()}).inserted_id
+
+        return f'File uploaded with ID: {file_id}'
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
